d96547c87a12c4997fc9a27e3a6e72c76cf1de9c
吕晶048807 <lvjing@renmaitech.com>
2020-09-10 19:26:12 +0800

    first commit

---
+++ src/main/java/com/api/remodeling/RemodelingApplication.java
@@ -0,0 +1,17 @@
+package com.api.remodeling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lvjing
+ * @date 2020/09/09
+ */
+@SpringBootApplication
+public class RemodelingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemodelingApplication.class, args);
+    }
+
+}
